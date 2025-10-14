0b75c4ac6d32d43dc938e32ac228ee50ecb0bbd0
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-10-14 23:44:22 +0530

    files modified

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 export const connectDB = async () =>{
     try{
-        await mongoose.connect("mongodb+srv://sampathwgw_db_user:uGw4XAYiOtc1HCNv@cluster0.lcuyegg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect(process.env.MONGO_URI)
 
         console.log("MONGODB CONNECTED SUCCESFULLY")
     } catch(error){
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import notesRouter from './routes/notesRouter.js';
 import {connectDB} from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+console.log(process.env.MONGO_URI);
 
 //const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 5001
 
 connectDB();
 
@@ -13,7 +19,7 @@ app.use('/api/notes', notesRouter);
 
 
 app.listen(5001, () => {
-    console.log("Server is running PORT: 5001");
+    console.log("Server is running PORT: ", PORT);
 });
 
 
